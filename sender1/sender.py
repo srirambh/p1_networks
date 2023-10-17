efddee0b434227525783124bb4e5071ca3c043db
Sriram Bharadwaj <sriram@Srirams-MacBook-Pro.local>
2023-10-17 15:49:02 -0500

    message changes

---
+++ sender1/sender.py
@@ -0,0 +1,78 @@
+import socket
+import time
+import argparse
+import struct
+from datetime import datetime
+
+def readFile(filename, length):
+    arr = []
+    with open(filename, "r+b") as f:
+        arr = bytearray(f.read())
+    return arr
+
+def receiveReq(serversocket):
+    data, address = serversocket.recvfrom(10000) # FIX
+    req = struct.unpack_from("!cII", data)
+    fileName = struct.unpack_from(f"!{req[2]}s", data, offset=9)[0].decode('utf-8')
+    print(fileName + " received.")
+    return fileName, address
+
+if __name__ == "__main__":
+
+    listArgs = []
+    
+    p = argparse.ArgumentParser()
+    p.add_argument("-p", "--port", type=int, choices=range(2049, 65537), help='Input port number for sender from ports between 2049 and 65536')
+    p.add_argument("-g", "--requester_port", type=int, choices=range(2049, 65537), help='Input requester port for receiver from ports between 2049 and 65536')
+    p.add_argument("-r", "--rate", type=int, help='Input rate at which packets are sent')
+    p.add_argument("-q", "--seq_no", type=int, help='Input sequence of packet exchange')
+    p.add_argument("-l", "--length", type=int, help='Input length of the payload')
+
+    args = p.parse_args()
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server.bind((socket.gethostname(), args.port))
+
+    filename, address = receiveReq(server)
+
+    try: 
+        byteArr = readFile(filename, args.length)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for i in range(0, len(byteArr), args.length):
+            packet = byteArr[i:min(i+args.length, len(byteArr))]
+            sock.sendto(struct.pack(f"!cII{len(bytes)}s", b'D', args.seq_no, len(byteArr), byteArr), (address, args.port))
+
+            print("DATA packet")
+            print("send time: ", datetime.utcnow())
+            print("requester addr: ", address)
+            print("sequence: ", args.seq_no)
+            print("length: ", packet.decode('utf-8')[:min(len(packet), 4)]) # in case packet len < 4
+            print()
+            args.seq_no += len(packet)
+            time.sleep(1.0 / args.rate)
+
+        sock.sendto(struct.pack(f"!cII",b'E',0,0), (address, args.port))
+
+        print("END packet")
+        print("send time: ", datetime.utcnow())
+        print("requester addr: ", address)
+        print("sequence: ", args.seq_no)
+        print("length: 0")
+        print("payload: ", packet.decode('utf-8')[:min(len(packet), 4)]) # in case packet len < 4
+
+    except: # in case the file does not exist with the sender
+
+        sock.sendto(struct.pack(f"!cII",b'E',0,0), (address, args.port))
+
+        print("END packet")
+        print("send time: ", datetime.utcnow())
+        print("requester addr: ", address)
+        print("sequence: ", args.seq_no)
+        print("length: 0")
+        print("payload: ", packet.decode('utf-8')[:min(len(packet), 4)]) # in case packet len < 4
+
+    server.close()
+
+
+
